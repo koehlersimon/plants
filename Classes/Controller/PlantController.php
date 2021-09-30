4e3c58bf56b12652cc5270805e2e572b42826c05
koehlersimon <info@simon-koehler.com>
2021-09-30 10:26:28 -0500

    Bugfix

--- Classes/Controller/PlantController.php
+++ Classes/Controller/PlantController.php
@@ -32,10 +32,8 @@ class PlantController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
         else{
             $this->view->assign('plants',$this->plantRepository->findAll());
         }
-        if($this->settings['list']['filter']['enabled']){
-            $plantFamlies = $this->familyRepository->findAll();
-            $this->view->assign('plantFamilies',$plantFamlies);
-        }
+        $plantFamlies = $this->familyRepository->findAll();
+        $this->view->assign('plantFamilies',$plantFamlies);
         $this->view->assign('settings',$this->settings);
     }
 
