1121c33a88a0f9c111f40d7d37dad00c55de6fc1
koehlersimon <info@simon-koehler.com>
2021-09-30 10:32:55 -0500

    Bugfix

--- Classes/Domain/Repository/FamilyRepository.php
+++ Classes/Domain/Repository/FamilyRepository.php
@@ -10,19 +10,4 @@ class FamilyRepository extends \TYPO3\CMS\Extbase\Persistence\Repository {
 		'title' => \TYPO3\CMS\Extbase\Persistence\QueryInterface::ORDER_ASCENDING
 	);
 
-	// Example for repository wide settings
-	public function initializeObject() {
-	  /** @var Typo3QuerySettings $querySettings */
-	  $querySettings = new Typo3QuerySettings();
-
-	  // don't add the pid constraint
-	  $querySettings->setRespectStoragePage(false);
-
-	  // add deleted rows to the result
-	  $querySettings->setIncludeDeleted(FALSE);
-
-	  // don't add sys_language_uid constraint
-	  $this->setDefaultQuerySettings($querySettings);
-	}
-
 }
